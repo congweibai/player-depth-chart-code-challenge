18cfda1e1e5e2fa776ba1e013f621c478f6adf3c
Wyatt Bai <wyatt.bai@caremappr.com.au>
2025-05-04 20:57:17 +0930

    feat: update ui

--- src/App.tsx
+++ src/App.tsx
@@ -34,41 +34,53 @@ function App() {
 
   return (
     <>
-      <Box
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-        marginBottom={2}
-      >
-        <FormControl
-          variant="filled"
-          sx={{ m: 1, minWidth: 220, background: "white" }}
+      <Box display="flex" justifyContent="space-between" alignItems="start">
+        <Box
+          sx={{
+            background: "white",
+            width: "420px",
+            marginBottom: "20px",
+            padding: "10px",
+            borderRadius: "5px",
+          }}
         >
-          <InputLabel id="game-type-select-label">{gameTypeText}</InputLabel>
-          <Select
-            labelId="game-type-select-label"
-            id="game-type-select"
-            value={gameType}
-            onChange={handleChange}
-            label={gameTypeText}
+          <AddPlayerToGameForm
+            addPlayerToGame={addPlayerToGame}
+            gameType={gameType}
+            gameData={gameData}
+          />
+        </Box>
+
+        <Box
+          display="flex"
+          justifyContent="left"
+          alignItems="center"
+          marginBottom={2}
+        >
+          <FormControl
+            variant="filled"
+            sx={{ m: 1, minWidth: 220, background: "white" }}
           >
-            <MenuItem value={GameType.NFL}> {GameType.NFL}</MenuItem>
-            <MenuItem value={GameType.SOCCER}> {GameType.SOCCER}</MenuItem>
-          </Select>
-        </FormControl>
+            <InputLabel id="game-type-select-label">{gameTypeText}</InputLabel>
+            <Select
+              labelId="game-type-select-label"
+              id="game-type-select"
+              value={gameType}
+              onChange={handleChange}
+              label={gameTypeText}
+            >
+              <MenuItem value={GameType.NFL}> {GameType.NFL}</MenuItem>
+              <MenuItem value={GameType.SOCCER}> {GameType.SOCCER}</MenuItem>
+            </Select>
+          </FormControl>
+        </Box>
       </Box>
+
       <DepthChartTable
         spotLabels={spotLabels}
         rows={gameData}
         handleRemovePlayer={onHandleRemovePlayer}
       />
-      <Box>
-        <AddPlayerToGameForm
-          addPlayerToGame={addPlayerToGame}
-          gameType={gameType}
-          gameData={gameData}
-        />
-      </Box>
     </>
   );
 }
